499a6741d2ead21b0101067ce61850f4ccad3ba2
trumk <nguyentrungtest2292003@gmail.com>
2024-02-22 11:15:55 +0700

    lab03 array practice

--- lab03/lab03.js
+++ lab03/lab03.js
@@ -1,4 +1,19 @@
 let a = 5
 let b = 6.9
+let x = 5;
+let y = 5;
+let z = 6;
+const aespa = ["winter","winter","karina","winter"];
 
-console.log(`Your a is: "`+ a +`" and b is: "`+ b+`"`)
+function addDataToParagraphs() {
+    // Truy xuất tất cả các phần tử p có class là "demo"
+    var paragraphs = document.querySelectorAll(".demo");
+  
+    // Lặp qua từng phần tử p và thêm dữ liệu từ mảng aespa vào
+    for (var i = 0; i < paragraphs.length; i++) {
+      paragraphs[i].textContent = aespa[i];
+    }
+  }
+
+console.log(`a is: "`+ a +`" and b is: "`+ b+`"`)
+console.log(`The first comparison between x and y is: "`+(x==y)+`", the second comparison between y and z is: "`+(y==z)+`"`)
